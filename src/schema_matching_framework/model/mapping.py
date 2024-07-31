843ef5f441e28d0f4827affa398eda64f96a7a1c
Mark S <mark@efficientsoftware.io>
2024-07-31 10:36:03 -0600

    scaffolding folder

---
+++ src/schema_matching_framework/model/mapping.py
@@ -0,0 +1,3 @@
+# TODO This file will define what a mapping looks like between a source and a destination
+# Also, for testing, we want a more advanced compare function.  We want to see how close
+# the mapping is to the correct mapping
